f5f5546fe5f769eab385a041d08e35910e79d922
Ammu <dudamithabi@gmail.com>
2025-06-17 14:49:42 +0530

    Add files via upload

---
+++ Addqueue.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class AddqueueA
+{
+public static void main(String[] args){
+Queue <Integer> q=new LinkedList< >();
+q.add(1);
+q.add(2);
+System.out.print(q);
+System.out.print(q.remove());
+System.out.print(q);
+}}
